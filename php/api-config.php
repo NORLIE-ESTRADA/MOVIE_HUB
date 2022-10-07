2fe79dfcc2714d431e185b9dba8fa84b3b53d958
Andrei <andreitogni@gmail.com>
2022-10-07 22:55:51 +0800

    search module

--- php/api-config.php
+++ php/api-config.php
@@ -1,5 +1,5 @@
 <?php
-$url = 'https://api.themoviedb.org/3/';
+$domain = 'https://api.themoviedb.org/3/';
 $api = 'api_key=ca97e80fa7f832d4b3cfb76bb842710f';
 
 ?>
--- php/search.php
+++ php/search.php
@@ -1,8 +1,31 @@
 <?php
-include ('api-config.php');
+include './api-config.php';
+
+
 
 $search = ('search/movie?');
-$query;
+$query = ('&query=');
+$rawinput = ('the avengers');
 
+# if ($_SERVER["REQUEST_METHOD"] == "POST"){
+#    $rawinput = $_POST["search"];
+# }
 
+$input = str_replace(' ', '+', $rawinput);
+
+$url = $domain.$search.$api.$query.$input;
+$json = file_get_contents($url);
+
+$data = json_decode($json);
+$count = count($data -> results);
+
+		for ($x = 0; $x < $count; $x++){
+			echo "Title: ". $data -> results[$x] -> title. "<br>"; 
+			echo "Image: ". $data -> results[$x] -> poster_path. "<br>";
+			echo "Description: ". $data -> results[$x] -> overview. "<br>";
+			echo "Release Date: ". $data -> results[$x] -> release_date. "<br>";
+			echo "Language: ". $data -> results[$x] -> original_language. "<br>"; 
+            echo "Vote Rating: ". ((($data -> results[$x] -> vote_average)*10)). "<br>"; 
+			echo "<br>";
+		}
 ?>
