70b2cb0a560ffb6d8d57e6ea6667d66ff9b942e3
Andrei <andreitogni@gmail.com>
2022-10-10 22:51:36 +0800

    last batch of back end edits

--- php/movie-data.php
+++ php/movie-data.php
@@ -1,17 +1,24 @@
 <?php
 include 'api-config.php';
-include '/image.php';
+include 'image.php';
 
-$discover = ('movie/');
-$movie_id;
-$genres = array();
-    echo "Title: ". $data -> title. "<br>"; 
+$movie = ('movie/');
+$movie_id = '642885';
+
+$url = $domain.$movie.$movie_id."?".$api;
+$json = file_get_contents($url);
+
+$data = json_decode($json);
+	$title = $data -> title;
 	$img = $imgurl.$data -> poster_path;
 	echo '<img alt="'.$title.'" src="' . $img . '">'. "<br>";
+	echo "Title: ". $title . "<br>"; 
     echo "Description: ". $data -> overview. "<br>";
-    for ($x = 0; $x < $count; $x++){
-        $genres[$x] = $data -> genres->name;
+	echo "Genres: ";
+    for ($x = 0; $x < count($data -> genres); $x++){
+        echo $data -> genres[$x]->name. " ";
     }
+	echo "<br>";
     echo "Release Date: ". $data  -> release_date. "<br>";
     echo "Language: ". $data -> original_language. "<br>"; 
     echo "Vote Rating: ". ((($data -> vote_average)*10)). "<br>"; 
